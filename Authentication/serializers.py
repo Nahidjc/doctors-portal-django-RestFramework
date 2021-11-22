8240335e72c641b2b86c5b85c6d2f13c7c9cbc42
Nahidjc <nahidjc80@gmail.com>
2021-11-22 22:57:34 +0600

    env

--- Authentication/serializers.py
+++ Authentication/serializers.py
@@ -1,3 +1,4 @@
+from Authentication.models import Account
 from rest_framework import serializers
 
 
@@ -7,3 +8,19 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Account
+        fields = ['email', 'username', 'password', 'password2']
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+    def save(self):
+        account = Account(email=self.validated_data['email'],
+                          username=self.validated_data['username'])
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+        if password != password2:
+            raise serializers.ValidationError(
+                {'password': 'Passwords must match.'})
+        account.set_password(password)
+        account.save()
+        return account
